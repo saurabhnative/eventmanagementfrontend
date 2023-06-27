8ca538c0b3f7532307504a9ee10c0442856f4f41
Saurabh Mhatre <codeclassifiers@gmail.com>
2023-06-27 22:44:51 +0530

    Private routes

--- src/components/events/layout.js
+++ src/components/events/layout.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Sidebar from './sidebar';
 import Header from './eventHeader';
+import { useRouter } from 'next/router';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 
 const Layout = ({ children }) => {
+  const router = useRouter();
+  useEffect(() => {
+    if (!localStorage.getItem('psg_auth_token')) {
+      router.push(`/`);
+    }
+  });
   return (
     <div>
       <Header />
